e858529edb2dd50242202d50e6de239f01968ce3
jihoho <xkftn94@naver.com>
2020-11-28 00:25:02 +0900

    테스트 코드 실행

---
+++ src/main/java/com/jihoho/myspring/springboot/web/HelloController.java
@@ -0,0 +1,13 @@
+package com.jihoho.myspring.springboot.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String Hello(){
+        return "hello";
+    }
+}
