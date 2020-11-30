3d65e49cbb1ff3382cce2e215899d3bdfe791cec
jihoho <xkftn94@naver.com>
2020-11-30 20:07:55 +0900

    jpa 테스트

---
+++ src/main/java/com/jihoho/myspring/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,7 @@
+package com.jihoho.myspring.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts,Long> {
+
+}
---
+++ src/main/java/com/jihoho/myspring/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,11 @@
+package com.jihoho.myspring.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+    private final String name;
+    private final int amount;
+}
