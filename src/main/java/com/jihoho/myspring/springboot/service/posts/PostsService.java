a52edeeab40a90dbdee5b3d67ad68639043b2bc5
jihoho <xkftn94@naver.com>
2020-12-10 20:50:58 +0900

    머스테치 활용하여 간단한 게시판 구현

--- src/main/java/com/jihoho/myspring/springboot/service/posts/PostsService.java
+++ src/main/java/com/jihoho/myspring/springboot/service/posts/PostsService.java
@@ -2,13 +2,18 @@ package com.jihoho.myspring.springboot.service.posts;
 
 import com.jihoho.myspring.springboot.domain.posts.Posts;
 import com.jihoho.myspring.springboot.domain.posts.PostsRepository;
+import com.jihoho.myspring.springboot.web.dto.PostsListResponseDto;
 import com.jihoho.myspring.springboot.web.dto.PostsResponseDto;
 import com.jihoho.myspring.springboot.web.dto.PostsSaveRequestDto;
 import com.jihoho.myspring.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 // autowired를 통해 주입 받지 않고 생선자로 주입받는 방식
 @RequiredArgsConstructor
@@ -35,5 +40,20 @@ public class PostsService {
                 .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없음. id="+id));
 
         return new PostsResponseDto(entity);
+    }
+
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+
+    @Transactional
+    public void delete(Long id ){
+        Posts posts = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id= "+id));
+        postsRepository.delete(posts);
     }
 }
---
+++ src/main/java/com/jihoho/myspring/springboot/web/IndexController.java
@@ -0,0 +1,36 @@
+package com.jihoho.myspring.springboot.web;
+
+import com.jihoho.myspring.springboot.service.posts.PostsService;
+import com.jihoho.myspring.springboot.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    private final PostsService postsService;
+
+    @GetMapping("/")
+    public String index(Model model){
+        model.addAttribute("posts", postsService.findAllDesc());
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
+    }
+
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto=postsService.findById(id);
+        model.addAttribute("post",dto);
+        System.out.println("id: "+dto.getId()+"content"+dto.getContent());
+        return "posts-update";
+    }
+}
