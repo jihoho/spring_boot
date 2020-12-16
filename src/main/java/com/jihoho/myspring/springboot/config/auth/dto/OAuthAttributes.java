29b2f2fc730543aa892a4a26fd62629556787ef9
jihoho <xkftn94@naver.com>
2020-12-16 16:09:19 +0900

    OAuth2로 네이버 로그인 구현

--- src/main/java/com/jihoho/myspring/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/jihoho/myspring/springboot/config/auth/dto/OAuthAttributes.java
@@ -26,6 +26,9 @@ public class OAuthAttributes {
 
     // OAuth2User에서 반환하는 사용자 정보는 Map 이기 때문에 값 하나하나를 변환해야만 함.
     public static OAuthAttributes of(String registartionId, String userNameAttributeName, Map<String ,Object> attributes){
+        if("naver".equals(registartionId)){
+            return ofNaver("id",attributes);
+        }
         return ofGoogle(userNameAttributeName,attributes);
     }
 
@@ -39,6 +42,16 @@ public class OAuthAttributes {
                 .build();
     }
 
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String,Object> attributes){
+        Map<String,Object> response=(Map<String, Object>) attributes.get("response");
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("picture"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
    /*
         User 엔티티를 생성함
         OAuthAttributes에서 엔티티를 생성하는 시점은 처음
